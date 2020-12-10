fddb7736bb02795a880504eb6476afa91ed12a0a
danielpineda <Dpcamacho97@gmail.com>
2020-12-10 16:26:22 -0600

    updated level
    updated level 1. added transition scene. updated earth level

--- Assets/Scripts/TransitionPoint.cs
+++ Assets/Scripts/TransitionPoint.cs
@@ -1,23 +1,28 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
-using UnityEditor;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
 
 public class TransitionPoint : MonoBehaviour
 {
 
-    [SerializeField] SceneAsset nextScene;
+    //[SerializeField] SceneAsset nextScene;
+    public string nextScene = null;
     bool isTriggered = false;
+
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (isTriggered)
             return;
-        
+
         if (collision.gameObject.layer == LayerMask.NameToLayer("Player"))
         {
+
             isTriggered = true;
-            GameManager.Instance.ChangeScene(nextScene.name);
+            GameManager.Instance.ChangeScene(nextScene);
         }
     }
 }
